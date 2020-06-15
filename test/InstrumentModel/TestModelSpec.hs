c5bb849afea145ca01faff290b56f5d695ff8936
Ettore Galli <ettore.galli@gmail.com>
2020-06-15 21:24:23 +0200

    Add tests

--- test/InstrumentModel/TestModelSpec.hs
+++ test/InstrumentModel/TestModelSpec.hs
@@ -4,10 +4,26 @@ module InstrumentModel.TestModelSpec (spec) where
     import InstrumentModel.Model
 
     spec :: Spec
-    spec = do
+    spec = do 
+        testgetInstrumentString
+        testGetNoMute
+          
+
+    testgetInstrumentString :: Spec
+    testgetInstrumentString = do
         describe "TestModel.Model" $ do
             it "getInstrumentStringKeyPosition 1" $ do
                 (getInstrumentStringKeyPosition (InstrumentString 1 0 (Fret 1))) `shouldBe` (1 :: Int)
             it "getInstrumentStringKeyPosition X" $ do
-                (fingeredFret (InstrumentString 1 0 X)) `shouldBe` X             
+                (fingeredFret (InstrumentString 1 0 X)) `shouldBe` X  
+
+
+    testGetNoMute :: Spec
+    testGetNoMute = do 
+        describe "TestModel.Model" $ do
+            it "testGetNoMute  " $ do
+                getNoMute testGetNoMuteCase `shouldBe` expectedNoMuteStrings
+                where 
+                    testGetNoMuteCase = [(InstrumentString 1 0 X), (InstrumentString 2 7 (Fret 1)), (InstrumentString 3 14 X), (InstrumentString 4 21 X)] 
+                    expectedNoMuteStrings = [(InstrumentString 2 7 (Fret 1))] 
 
